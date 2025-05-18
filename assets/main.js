1fb545876489b9b254000055e8b4e402d0bddb1c
Rajneshgupta <131360073+Rajneshgupta@users.noreply.github.com>
2025-05-18 05:05:47 -0700

    fixed responsive

---
+++ assets/main.js
@@ -0,0 +1,77 @@
+let flag = 0;
+
+setInterval(function control(x) {
+    flag = flag + 1;
+    show(flag);
+}, 2000);
+
+function show(num) {
+    let slides = document.getElementsByClassName("wraptext");
+
+    if (num == slides.length) {
+        flag = 0;
+        num = 0;
+    }
+    if (num < 0) {
+        flag = slides.length - 1;
+        num = slides.length - 1;
+    }
+
+    for (var y of slides) {
+        y.style.display = "none";
+    }
+
+    slides[num].style.display = "block";
+}
+
+var responsiveSlider = function () {
+    var slider = document.getElementById("slide");
+    var sliderWidth = slider.offsetWidth;
+    var slideList = document.getElementById("slideWrap");
+    var count = 1;
+    var items = slideList.querySelectorAll("li").length;
+    var prev = document.getElementById("prev");
+    var next = document.getElementById("next");
+
+    window.addEventListener('resize', function () {
+        sliderWidth = slider.offsetWidth;
+    });
+
+    var prevSlide = function () {
+        if (count > 1) {
+            count = count - 2;
+            slideList.style.left = "-" + count * sliderWidth + "px";
+            count++;
+        } else if (count = 1) {
+            count = items - 1;
+            slideList.style.left = "-" + count * sliderWidth + "px";
+            count++;
+        }
+    };
+
+    var nextSlide = function () {
+        if (count < items) {
+            slideList.style.left = "-" + count * sliderWidth + "px";
+            count++;
+        } else if (count = items) {
+            slideList.style.left = "0px";
+            count = 1;
+        }
+    };
+
+    next.addEventListener("click", function () {
+        nextSlide();
+    });
+
+    prev.addEventListener("click", function () {
+        prevSlide();
+    });
+
+    setInterval(function () {
+        nextSlide()
+    }, 2000);
+};
+
+window.onload = function () {
+    responsiveSlider();
+}
